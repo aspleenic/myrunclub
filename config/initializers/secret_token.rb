3e479123de08f45258337ad8bf320647a41f281e
PJ Hagerty <pjhagerty@gmail.com>
2012-06-13 10:15:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrunclub::Application.config.secret_token = 'bc0261115cc390d05bea0c59905bdfebcc18bd0fa2de37bab387fc12ed03e1787fdff5168fa489d5e5ede5e2def6c64ef9333daa90c50933c50ea754e0ddab15'
